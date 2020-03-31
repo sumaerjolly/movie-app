3f5365ed89d8724eca90a1dd0f98416664a27451
Sumaer Jolly <sumaerjolly@gmail.com>
2020-03-31 18:43:50 +0530

    add api call

--- src/App.js
+++ src/App.js
@@ -1,14 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 import Search from './components/Search';
 
 function App() {
+  const [state, setState] = useState({
+    searchQuery: '',
+    results: [],
+    selected: {}
+  });
+  const apiUrl = 'http://www.omdbapi.com/?apikey=69ddb86f';
+
+  const search = e => {
+    if (e.key === 'Enter') {
+      axios
+        .get(`${apiUrl}&s=${state.searchQuery}`)
+        .then(response => console.log(response))
+        .catch(error => console.log('api error is', error));
+    }
+  };
+
+  const handleInput = e => {
+    let searchQuery = e.target.value;
+    setState(prevState => {
+      return { ...prevState, searchQuery };
+    });
+  };
+
   return (
     <div className="App">
       <header>
         <h1>Movie App</h1>
       </header>
       <main>
-        <Search />
+        <Search handleInput={handleInput} search={search} />
       </main>
     </div>
   );
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
-function Search() {
+function Search({ handleInput, search }) {
   return (
     <section className="searchbox-wrap">
       <input
         type="text"
         placeholder="Search for any movie..."
         className="searchbox"
+        onChange={handleInput}
+        onKeyPress={search}
       />
     </section>
   );
